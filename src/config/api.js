41bde62fce1c1dd284ae152219b9b7991ac18a5b
TheHappyCookieHour <c28_09_98@hotmail.com>
2023-07-27 19:10:17 +0000

    styled javascript

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const uri = "https://pokeapi.co/api/v2"
 
-export const getPokemon = async (endpoint, params)=> {
-    await new Promise(r => setTimeout(r, 500));
-    return await axios.get(`${uri}/${endpoint}${params}`);
+export const getPokemon = async (endpoint, params) => {
+  await new Promise(r => setTimeout(r, 500));
+  return await axios.get(`${uri}/${endpoint}${params}`);
 };
